02e3b441591eb54063994e14fdadaafa40596616
kalle <kmthorsnes@gmail.com>
2019-02-06 23:16:58 +0100

    wip!

--- js/scripts.js
+++ js/scripts.js
@@ -4,33 +4,22 @@ $(document).ready(function() { // Starts jQuery
         url: 'https://randomuser.me/api/?results=12',
         dataType: 'json',
         success: function (data) {
-            var user = data.results[0].user;
+            console.log(data);
             console.log(data.results);
-            $('#gallery').append('<div class="card"></div>');
+            console.log(data.results[0].name.first);
+            
+            $('#gallery').append("<div class='card'></div>");
             $('.card').append('<div class="card-img-container"></div>');
             $('.card-img-container').append('<img class="card-img" src="https://placehold.it/90x90" alt="profile picture"></div>');
             
-            $.each( data, function(key, value  ) {
-                // Assign Data
-                // Image
-                console.log(email.email)
-                // var image = '<img src=' + user.picture + '>';
-                // $(".card-img-container'").append(image);
-                // $("#userP").append(image);
-                // First and Last Name
-                // var name = = '<p> + (user.name.first) +  </p>' '>';
-
-                // Email
-                // City or location
-
-                
-                // $('#user_value').html(user.name.first + ' ' + user.name.last);
-        
-                // $('li[data-label="location"]').attr('data-value', user.location.street);
-                //$('#gallery').html(image);
-                })
+            $.each(data.results, function(i) {
+            console.log(data.results[i].name.first); // will alert each value
+            });
         }
+           
+        
     }) 
 }); // Ends jQuery
 
 // https://randomuser.me/api/?results=12
+//https://codepen.io/jaywolters/pen/GpZvLr
