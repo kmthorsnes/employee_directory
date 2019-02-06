c24444ba7778cad39168fa1eca427b26777d9938
kalle <kmthorsnes@gmail.com>
2019-02-06 14:49:56 +0100

    wip

--- js/scripts.js
+++ js/scripts.js
@@ -9,32 +9,28 @@ $(document).ready(function() { // Starts jQuery
             $('#gallery').append('<div class="card"></div>');
             $('.card').append('<div class="card-img-container"></div>');
             $('.card-img-container').append('<img class="card-img" src="https://placehold.it/90x90" alt="profile picture"></div>');
-
-
-;
-            //$.each( obj, function( key, value ) {
-            // Assign Data
-            // Image
-            // var image = '<img src=' + user.picture + '>';
-            // $("#user_photo").append(image);
-            // $("#userP").append(image);
-            // First and Last Name
-            // var name = = '<p> + (user.name.first) +  </p>' '>';
-
-            // Email
-            // City or location
-
             
-            // $('#user_value').html(user.name.first + ' ' + user.name.last);
-    
-            // $('li[data-label="location"]').attr('data-value', user.location.street);
-            //$('#gallery').html(image);
-    
-        }
-    });
-  
-    
-}); // Ends jQuery
+            $.each( data, function(key, value  ) {
+                // Assign Data
+                // Image
+                console.log(email.email)
+                // var image = '<img src=' + user.picture + '>';
+                // $(".card-img-container'").append(image);
+                // $("#userP").append(image);
+                // First and Last Name
+                // var name = = '<p> + (user.name.first) +  </p>' '>';
 
+                // Email
+                // City or location
+
+                
+                // $('#user_value').html(user.name.first + ' ' + user.name.last);
+        
+                // $('li[data-label="location"]').attr('data-value', user.location.street);
+                //$('#gallery').html(image);
+                })
+        }
+    }) 
+}); // Ends jQuery
 
 // https://randomuser.me/api/?results=12
