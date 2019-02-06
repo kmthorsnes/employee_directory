f0b95b517fb44a66fddd663604ae4a2b8dfa4da9
kalle <kmthorsnes@gmail.com>
2019-02-06 23:33:43 +0100

    slowly

--- js/scripts.js
+++ js/scripts.js
@@ -11,10 +11,28 @@ $(document).ready(function() { // Starts jQuery
             $('#gallery').append("<div class='card'></div>");
             $('.card').append('<div class="card-img-container"></div>');
             $('.card-img-container').append('<img class="card-img" src="https://placehold.it/90x90" alt="profile picture"></div>');
+            $('.card').append('<div class="card-info-container"></div>');
+            $('.card-info-container').append('<h3 id="name" class="card-name cap">first last</h3>');
+            $('.card-info-container').append('<p class="card-text">email</p>');
+            $('.card-info-container').append('<p class="card-text cap">city, state</p>');
+            
+            
+        
+
+
             
             $.each(data.results, function(i) {
-            console.log(data.results[i].name.first); // will alert each value
-            });
+            console.log(data.results[i].name.first)
+            let firstName = data.results[i].name.first;
+            let lastName = data.results[i].name.last;
+            let email = data.results[i].email;
+            let mediumPhoto = data.results[i].picture.medium;
+            
+            $('.card').append('<div class="card-img-container"></div>');
+            $('.card-img-container').append('<img class="card-img" src='+ mediumPhoto + ' alt="profile picture"></div>');    
+            $('.card-info-container').append('<h3 id="name" class="card-name cap">'+ firstName +" " + lastName +'</h3>');
+            $('.card-info-container').append('<p class="card-text">'+email+'</p>');    
+        });
         }
            
         
