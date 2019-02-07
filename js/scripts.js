b1b3ddb371f2965193e57fb0b7ef1b15ab9c1273
kalle <kmthorsnes@gmail.com>
2019-02-07 12:34:04 +0100

    starting on adding modal

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,18 @@ $(document).ready(function() { // Starts jQuery
  
     //helper methods 
     
+    function createModal (e) {
+
+        $('#gallery').append('<div class="modal-container"></div>');
+        $('.modal-container').append('<div class="card-img-container"></div>');
+        $('.card-img-container').append('<img class="card-img" src="https://placehold.it/90x90" alt="profile picture"></img>');
+
+
+    }
+
+    //funksjon for å lage modalvindu
+
+    // clickhørere. 
 
     
     
@@ -13,27 +25,11 @@ $(document).ready(function() { // Starts jQuery
 
 
 
+
     $.ajax({
         url: 'https://randomuser.me/api/?results=12',
         dataType: 'json',
         success: function (data) {
-            console.log(data);
-            console.log(data.results);
-            console.log(data.results[0].name.first);
-            
-            // $('#gallery').append("<div class='card'></div>");
-            // $('.card').append('<div class="card-img-container"></div>');
-            // $('.card-info-container').append('<img class="card-img"</div>');
-            // $('.card-img-container').append('<img class="card-img" src="https://placehold.it/90x90" alt="profile picture"></div>');
-            // $('.card').append('<div class="card-info-container"></div>');
-            // $('.card-info-container').append('<h3 id="name" class="card-name cap">first last</h3>');
-            // $('.card-info-container').append('<p class="card-text">email</p>');
-            // $('.card-info-container').append('<p class="card-text cap">city, state</p>');
-            
-            
-        
-
-
             
             $.each(data.results, function(i) {
             console.log(data.results[i].name.first)
@@ -62,6 +58,9 @@ $(document).ready(function() { // Starts jQuery
         });
         }
            
+
+
+
         
     }) 
 }); // Ends jQuery
