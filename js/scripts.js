6b64082a675ae865fca48ed82155af02479ae6d9
kalle <kmthorsnes@gmail.com>
2019-02-07 17:46:50 +0100

    work in progress

--- js/scripts.js
+++ js/scripts.js
@@ -79,8 +79,40 @@ $(document).ready(function() {
           .addClass(".card-info-container");
       });
 
-      // Creating modal
-   
+    //  Creating modal
+      
+
+
+    // Creating click event
+    $('.card').click(function() {
+        $("#gallery").append(
+            '<div class="modal-container">'+
+             '<div class="modal">'          + 
+              '<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>'+
+                 '<div class="modal-info-container">'+
+                      '<img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">'+
+                      '<h3 id="name" class="modal-name cap">name</h3>'+
+                      '<p class="modal-text">email</p>'+
+                      '<p class="modal-text cap">'+data.results[$(this)].location.city+'</p>'+
+                      '<hr>'+
+                      '<p class="modal-text">(555) 555-5555</p>'+
+                      '<p class="modal-text">123 Portland Ave., Portland, OR 97204</p>'+
+                      '<p class="modal-text">Birthday: 10/21/2015</p>'+
+                  '</div>'+
+              '</div>'+
+             
+              '<div class="modal-btn-container">'+
+                  '<button type="button" id="modal-prev" class="modal-prev btn">Prev</button>'+
+                  '<button type="button" id="modal-next" class="modal-next btn">Next</button>'+
+              '</div>'+
+          '</div>');
+          $('.modal-close-btn').click(function() {
+              $('.modal-container').remove();
+          });
+ 
+    });
+
+
     }
   });
 }); // Ends jQuery
