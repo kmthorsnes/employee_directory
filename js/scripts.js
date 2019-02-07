30766ac54513b94cf444d23b4372bfa4f140c665
kalle <kmthorsnes@gmail.com>
2019-02-07 01:07:18 +0100

    wip

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,14 @@ $(document).ready(function() { // Starts jQuery
             console.log(data.results);
             console.log(data.results[0].name.first);
             
-            $('#gallery').append("<div class='card'></div>");
-            $('.card').append('<div class="card-img-container"></div>');
-            $('.card-img-container').append('<img class="card-img" src="https://placehold.it/90x90" alt="profile picture"></div>');
-            $('.card').append('<div class="card-info-container"></div>');
-            $('.card-info-container').append('<h3 id="name" class="card-name cap">first last</h3>');
-            $('.card-info-container').append('<p class="card-text">email</p>');
-            $('.card-info-container').append('<p class="card-text cap">city, state</p>');
+            // $('#gallery').append("<div class='card'></div>");
+            // $('.card').append('<div class="card-img-container"></div>');
+            // $('.card-info-container').append('<img class="card-img"</div>');
+            // $('.card-img-container').append('<img class="card-img" src="https://placehold.it/90x90" alt="profile picture"></div>');
+            // $('.card').append('<div class="card-info-container"></div>');
+            // $('.card-info-container').append('<h3 id="name" class="card-name cap">first last</h3>');
+            // $('.card-info-container').append('<p class="card-text">email</p>');
+            // $('.card-info-container').append('<p class="card-text cap">city, state</p>');
             
             
         
@@ -27,11 +28,12 @@ $(document).ready(function() { // Starts jQuery
             let lastName = data.results[i].name.last;
             let email = data.results[i].email;
             let mediumPhoto = data.results[i].picture.medium;
-            
-            $('.card').append('<div class="card-img-container"></div>');
-            $('.card-img-container').append('<img class="card-img" src='+ mediumPhoto + ' alt="profile picture"></div>');    
-            $('.card-info-container').append('<h3 id="name" class="card-name cap">'+ firstName +" " + lastName +'</h3>');
-            $('.card-info-container').append('<p class="card-text">'+email+'</p>');    
+            $('#gallery').append("<div class='card"+i+"'></div>");
+            $('.card'+i).append('<div class="card-img-container'+i+'"></div>');
+            //$('.card"+i+"').append('<div class="card-info-container'+i+'"></div>');
+            //$('.card-img-container').append('<img class="card-img" src='+ mediumPhoto + ' alt="profile picture"></div>');    
+           // $('.card-info-container').append('<h3 id="name" class="card-name cap">'+ firstName +" " + lastName +'</h3>');
+          // $('.card-info-container'+i+'').append('<p class="card-text'+i+'">'+email+'</p>');    
         });
         }
            
